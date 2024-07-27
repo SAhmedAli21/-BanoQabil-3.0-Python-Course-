0a929f2448db8def049f226b3697800a2892eb4c
SAhmedAli21 <aleahmed21005@gmail.com>
2024-07-27 08:29:00 -0700

    Add files via upload

---
+++ project.py
@@ -0,0 +1,13 @@
+# Syed Ahmed Ali
+# 69373
+# aleahmed21005@gmail.com
+
+s = input("Enter the value: ")
+
+reverse = s[::-1]
+
+if ( s == reverse ):
+    print("yes it is palindrome")
+else:
+    print("No it is not palindrome")
+    
